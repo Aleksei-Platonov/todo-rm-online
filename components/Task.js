a879b81dadccd12d73098e8088afd67b58bbb1b1
Aleksei-Platonov <platonov.fishman1986@gmail.com>
2023-05-20 20:25:04 +0200

    upgrade Task file

--- components/Task.js
+++ components/Task.js
@@ -14,37 +14,37 @@ const Task = (props) => {
 }
 
 const styles = StyleSheet.create({
-    item:{
-        backgroundColor:'#fff',
-        padding:15,
-        borderRadius:10,
-        flexDirection:'row',
-        alignItems:'center',
-        justifyContent:'space-between',
-        marginBottom:20,
+    item: {
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20,
     },
-    itemLeft:{
-        flexDirection:'row',
-        alignItems:'center',
-        flexWrap:'wrap',
+    itemLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap',
     },
-    square:{
-        width:24,
-        height:24,
-        backgroundColor:'#55bcf6',
-        opacity:0.4,
-        borderRadius:5,
-        marginRight:15,
+    square: {
+        width: 24,
+        height: 24,
+        backgroundColor: '#55bcf6',
+        opacity: 0.4,
+        borderRadius: 5,
+        marginRight: 15,
     },
-    itemText:{
-        maxWidth:'80%',
+    itemText: {
+        maxWidth: '80%',
     },
-    circular:{
-        width:12,
-        height:12,
+    circular: {
+        width: 12,
+        height: 12,
         backgroundColor: '#55bcf6',
         borderWidth: 2,
-        borderRadius:5,
+        borderRadius: 5,
     }
 });
 
